7d5cf9cf1c6dc4a466d58ec70cb99e1e7610f692
juliogitdev <juliovieira035@gmail.com>
2025-09-07 19:36:35 -0300

    project initializer

---
+++ SmsGateway/src/test/java/com/example/SmsGateway/SmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmsGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
